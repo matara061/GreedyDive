1358cb5167f541dc5f10dbc3bd0468f8b3a3fedc
Nyct1bius <theo.yamada02@gmail.com>
2024-04-21 21:53:26 -0300

    Nova Cena de Loop

--- Assets/Scripts/InfiniteBackground.cs
+++ Assets/Scripts/InfiniteBackground.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InfiniteBackground : MonoBehaviour
 {
-    public float backgroundSpeed;
-    public Renderer backgroundRenderer;
+    public bool isSpecialScene;
+
+    public GameObject nextLoop;
+
+    public float loopSpawnTimer;
+
+    private Transform myPosition;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        loopSpawnTimer = 6f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        backgroundRenderer.material.mainTextureOffset += new Vector2(backgroundSpeed * Time.deltaTime, 0f);
+        if (loopSpawnTimer <= 0)
+        {
+            Instantiate(nextLoop, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        }
     }
 }
