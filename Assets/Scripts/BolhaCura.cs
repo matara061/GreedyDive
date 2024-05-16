fe7f5c2d963220285bb5573a673c9baea3518012
Matheus Ferreira <matheusfsantos.152@gmail.com>
2024-05-16 15:43:27 -0300

    ajustes

--- Assets/Scripts/BolhaCura.cs
+++ Assets/Scripts/BolhaCura.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BolhaCura : MonoBehaviour
 {
 
     private Player player;
+
+    public GameObject floatingTxt; // O prefab da mensagem de aviso
 
     void Start()
     {
@@ -17,7 +20,21 @@ public class BolhaCura : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             player.Cura();
+            ShowMessage("+++");
             Destroy(gameObject);
         }
+    }
+
+    private void ShowMessage(string warning)
+    {
+        // Instancia a mensagem de aviso
+        GameObject message = Instantiate(floatingTxt, this.transform);
+
+        // Configura a mensagem
+        TextMeshProUGUI text = message.GetComponent<TextMeshProUGUI>();
+        text.text = warning;
+
+        // Destrua a mensagem após alguns segundos
+        Destroy(message, 3f);
     }
 }
