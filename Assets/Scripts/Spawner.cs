17d8faf0e251ec894c58f8465b6ad23a6a8632c8
Nyct1bius <theo.yamada02@gmail.com>
2024-05-20 10:46:26 -0300

    Shark Atum e Bacalhau

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,116 +4,42 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    private bool spawnFish = false, spawnObstacle = false;
+    public List<GameObject> prefabs;
+    private GameObject lastSpawned;
+    public GameObject ItemBox;
+    private int sameSpawnCount = 0;
 
-    private bool canSpawn = true;
+    public int seconds;
 
-    private float obstacleTypeNumber, fishTypeNumber, diceRoll;
-
-    public GameObject pacu, bagre, tubarao, barril;
-
-    public bool tier1Spawner, tier2Spawner, tier3Spawner, isRockSpawner;
-
-    public float spawnerCooldown;
-
-    private void Awake()
-    {
-        if (tier1Spawner)
-        {
-            diceRoll = Random.Range(0, 10);
-        }
-        if (tier2Spawner)
-        {
-            diceRoll = Random.Range(0, 20);
-        }
-        if (tier2Spawner)
-        {
-            diceRoll = Random.Range(0, 10);
-        }
-    }
-
-    // Start is called before the first frame update
     void Start()
     {
-        if (!isRockSpawner)
-        {
-            switch (diceRoll)
-            {
-                case 0:
-                case 1:
-                case 2:
-                    spawnObstacle = true;
-                    obstacleTypeNumber = Random.Range(0, 3);
-                    break;
-                case 3:
-                case 4:
-                case 5:
-                case 6:
-                case 7:
-                case 8:
-                case 9:
-                    spawnFish = true;
-                    fishTypeNumber = Random.Range(0, 2);
-                    break;
-
-            }
-        }
-
-        spawnerCooldown = Random.Range(5, 11);
+        StartCoroutine(SpawnRandom());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnRandom()
     {
-        if (canSpawn)
+        while (true)
         {
-            spawnerCooldown = Random.Range(5, 11);
+            yield return new WaitForSeconds(seconds);
 
-            if (!isRockSpawner)
+            GameObject toSpawn;
+            do
             {
-                if (spawnFish)
-                {
-                    switch (fishTypeNumber)
-                    {
-                        case 0:
-                            Instantiate(pacu, transform.position, Quaternion.identity);
-                            canSpawn = false;
-                            break;                           
-                        case 1:
-                            Instantiate(bagre, transform.position, Quaternion.identity);
-                            canSpawn = false;
-                            break;
-                    }
-                }
-                if (spawnObstacle)
-                {
-                    switch (obstacleTypeNumber)
-                    {
-                        case 0:
-                        case 1:
-                            Instantiate(barril, transform.position, Quaternion.identity);
-                            canSpawn = false;
-                            break;
-                        case 2:
-                            Instantiate(tubarao, transform.position, Quaternion.identity);
-                            canSpawn = false;
-                            break;
-                    }
-                }
+                toSpawn = prefabs[Random.Range(0, prefabs.Count)];
             }
-        }
-        else
-        {
-            spawnerCooldown -= Time.deltaTime;
+            while (lastSpawned == toSpawn && sameSpawnCount >= 3);
 
-            diceRoll = Random.Range(0, 10);
-            obstacleTypeNumber = Random.Range(0, 3);
-            fishTypeNumber = Random.Range(0, 2);
-
-            if (spawnerCooldown <= 0)
+            if (lastSpawned == toSpawn)
             {
-                canSpawn = true;
+                sameSpawnCount++;
             }
+            else
+            {
+                sameSpawnCount = 0;
+            }
+
+            lastSpawned = Instantiate(toSpawn, transform.position, Quaternion.identity);
+            lastSpawned.transform.parent = ItemBox.transform; // Define o objeto vazio como o pai
         }
     }
 }
